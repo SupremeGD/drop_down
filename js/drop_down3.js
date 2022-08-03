5dbb7d5ce976a2daa70507c5ee32a03951815afd
SupremeGD <759296837@qq.com>
2022-08-03 15:44:58 +0800

    更新说明

---
+++ js/drop_down3.js
@@ -0,0 +1,7 @@
+var drop_button = document.querySelector(".dropdown-button");
+var content2 = document.querySelector(".dropdown_content");
+
+drop_button.onclick = function() {
+    // content = document.querySelector(".dropdown_content");
+    content2.style.dispaly = "block";
+}
